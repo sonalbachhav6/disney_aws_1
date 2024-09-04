dd8c7923408c686136250219efaf08cbca7e97bc
sonalbachhav6 <sonalbachhav6@gmail.com>
2024-09-04 10:29:12 +0100

    disney aws handson

---
+++ src/main/java/com/example/disney/disney_aws_1/DisneyAws1Application.java
@@ -0,0 +1,13 @@
+package com.example.disney.disney_aws_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyAws1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyAws1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/disney/disney_aws_1/DisneyAws1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.disney.disney_aws_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyAws1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
