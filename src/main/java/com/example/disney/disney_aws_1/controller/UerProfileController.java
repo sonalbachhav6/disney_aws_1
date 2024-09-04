04342d00ccf86b30f440f1338fc6eb505973864d
sonalbachhav6 <sonalbachhav6@gmail.com>
2024-09-04 12:11:59 +0100

    added base packages

---
+++ src/main/java/com/example/disney/disney_aws_1/controller/UerProfileController.java
@@ -0,0 +1,15 @@
+package com.example.disney.disney_aws_1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+
+("/api/subscrptiption")
+@RequestMapping
+
+public class UerProfileController {
+	
+
+}
